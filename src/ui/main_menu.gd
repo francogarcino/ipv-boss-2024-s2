b7c31004b0d79a23e4797601bbbebe5b7e10f49c
l_ziege <lucasziegemann@gmail.com>
2024-11-14 19:06:18 -0300

    Se añade musica al juego

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 signal start()
 
+@onready var game_sound: AudioStreamPlayer2D = $GameSound
+
 func _ready() -> void:
 	_show()
 
@@ -13,6 +15,7 @@ func _on_start_button_pressed() -> void:
 	hide()
 	get_tree().paused = false
 	emit_signal("start")
+	game_sound.play()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
